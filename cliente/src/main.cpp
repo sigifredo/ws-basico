491148947a36255da7db74e0b985d709e5bbceea
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2013-04-14 04:46:03 -0500

    Nos conectamos satisfactoriamente con el servidor.
    Enviamos una petición y capturamos satisfactoriamente la respuesta.

--- cliente/src/main.cpp
+++ cliente/src/main.cpp
@@ -8,12 +8,18 @@ int main()
 {
     servidorPortBinding soapObj;
     
-    ns1__hello *hola = new ns1__hello;
+    ns1__hello *request = new ns1__hello;
     ns1__helloResponse *response = new ns1__helloResponse;
-    
-    soapObj.__ns1__hello(hola, response);
 
-    std::cout << response->return_ << std::endl;
+    std::string nombre("Sigifredo");
+    request->name = &nombre;
+
+    soapObj.__ns1__hello(request, response);
+
+    std::cout << *response->return_ << std::endl;
+
+    delete request;
+    delete response;
 
     return 0;
 }
